742c516566a2447aa48fffdbed19f0fbcf5e7862
Hernandez Hernandez <omar.hernandez.hernandez.contractor@bbva.com>
2018-08-30 10:17:23 -0500

    Project with thymeleaf

---
+++ alumnos/src/main/java/com/curso/alumnos/configuration/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.curso.alumnos.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	protected void configureGlobal(AuthenticationManagerBuilder auth)throws Exception{
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http)throws Exception{
+		http.authorizeRequests()
+			.antMatchers("/resources/**").permitAll()
+			.antMatchers("/h2-console").permitAll()
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.antMatchers("/alumnos/**").access("hasAnyRole('ROLE_ADMIN', 'ROLE_ALUMNOS')")
+			.antMatchers("/profesor/**").access("hasAnyRole('ROLE_ADMIN', 'ROLE_PROFESOR')")
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+			.usernameParameter("username")
+			.passwordParameter("password")
+			.loginProcessingUrl("/j_spring_security_check")
+			.loginPage("/login").failureUrl("/login?error=true")
+			.defaultSuccessUrl("/home")
+			.permitAll()
+			.and()
+			.logout()
+			.permitAll();
+	}
+	
+	@Override
+	public void configure (WebSecurity web) throws Exception{
+		web
+		.ignoring()
+		.antMatchers("/resources/**","/static/**","/css/**","/js/**","/images/**","/h2-console/**");
+	}
+}
---
+++ alumnos/src/main/java/com/curso/alumnos/configuration/service/UserDetailsServiceImpl.java
@@ -0,0 +1,42 @@
+package com.curso.alumnos.configuration.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+import com.curso.alumnos.dao.UsuarioDao;
+import com.curso.alumnos.dto.UsuarioDto;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioDao usuarioDao;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	 
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		 System.out.println(bCryptPasswordEncoder.encode("123"));	
+		 UsuarioDto usuarioDto = usuarioDao.findByUsuario(username);
+		 Set<GrantedAuthority> grantedAuthorities = new HashSet();
+		 grantedAuthorities.add(new SimpleGrantedAuthority(usuarioDto.getRol().getNombre()));
+		 
+		User user = new User(usuarioDto.getUsuario(),usuarioDto.getPassword(), grantedAuthorities);
+		return user;
+	}
+
+	
+}
